35008671e8c4089750a9bb9654581a7b8bed1c4a
Brett Sheffield <brett@gladserv.com>
2019-08-19 09:48:46 +0100

    store mld group and interface state

--- src/mldspy.c
+++ src/mldspy.c
@@ -1,3 +1,4 @@
+#include "color.h"
 #include "mldspy.h"
 #include <arpa/inet.h>
 #include <assert.h>
@@ -7,7 +8,8 @@
 #include <netdb.h>
 #include <netinet/icmp6.h>
 #include <netinet/in.h>
-#include <stddef.h>
+#include <signal.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,8 +17,6 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-#include <linux/if_ether.h>
 
 #define BUFSIZE 1500
 
@@ -46,11 +46,36 @@
 
 #define MLD2_HEADER_SIZE 8
 
-struct mld_group {
-	struct in6_addr	mar_address;	/* Multicast Address */
-	/* TODO: interface */
-	/* TODO: timer (LLQT) */
+#if !__USE_KERNEL_IPV6_DEFS
+/* IPv6 packet information.  */
+struct in6_pktinfo
+{
+	struct in6_addr ipi6_addr;  /* src/dst IPv6 address */
+	unsigned int ipi6_ifindex;  /* send/recv interface index */
 };
+#endif
+
+typedef enum {
+	FILTER_MODE_INCLUDE,
+	FILTER_MODE_EXCLUDE,
+} filter_mode_t;
+
+typedef struct mld_source_t {
+	struct in6_addr		addr;		/* source address */
+	uint32_t		src_timer;	/* source timer */
+	struct mld_group_t *	group;		/* parent MLD group */
+	struct mld_source_t *	next;
+} mld_source_t;
+
+typedef struct mld_group_t {
+	struct in6_addr		addr;		/* Multicast Address */
+	uint32_t		filter_timer;	/* Filter Timer */
+	int			iface;		/* Network Interface */
+	filter_mode_t		mode;		/* Router Filter Mode */
+	struct mld_source_t *	src_inc;	/* Include (Requested) List */
+	struct mld_source_t *	src_exc;	/* Exclude List */
+	struct mld_group_t *	next;
+} mld_group_t;
 
 /* Multicast Address Record */
 struct mar {
@@ -58,7 +83,6 @@ struct mar {
 	uint8_t		mar_auxlen;	/* Aux Data Len */
 	uint16_t	mar_sources;	/* Number of Sources */
 	struct in6_addr	mar_address;	/* Multicast Address */
-	//struct in6_addr src_address;	/* source address */
 } __attribute__((__packed__));
 
 /* Version 2 Multicast Listener Report Message */
@@ -71,34 +95,135 @@ struct mld2 {
 	struct mar	mld2_mar;	/* First MCast Address Record */
 } __attribute__((__packed__));
 
-void * process_multicast_address_record(struct mar *mrec)
+static int sock = 0;
+static mld_group_t *groups = NULL;
+
+/* free generic linked-list */
+void free_list(void *g)
+{
+	for (mld_group_t *ptr = g; g; ptr = g) {
+		g = ptr->next;
+		free(ptr);
+	}
+	g = NULL;
+}
+
+/* free group linked-list */
+void free_group(mld_group_t *g)
+{
+	for (mld_group_t *ptr = g; g; ptr = g) {
+		g = ptr->next;
+		free_list(ptr->src_inc);
+		free_list(ptr->src_exc);
+		free(ptr);
+	}
+	g = NULL;
+}
+
+/* return new or matching group record */
+mld_group_t * group_record(struct in6_addr addr, int iface)
+{
+	mld_group_t *g, *t;
+
+	if (groups) {
+		/* find matching group+interface in linked list */
+		for (t = groups; t->next; t = t->next) {
+			/* match found, return it */
+			if ((memcmp(&(t->addr), &addr, sizeof(struct in6_addr)) == 0)
+			&& (t->iface == iface))
+			{
+				return t;
+			}
+		}
+		/* no match, allocate new struct */
+		g = calloc(1, sizeof(struct mld_group_t));
+		t->next = g;
+	}
+	else {
+		/* first time, allocate new struct */
+		g = calloc(1, sizeof(struct mld_group_t));
+		groups = g;
+	}
+	g->addr = addr;
+	g->iface = iface;
+
+	return g;
+}
+
+void handle_sigint()
+{
+	free_group(groups);
+	close(sock);
+
+	_exit(0);
+}
+
+/* extract interface number from ancillary control data */
+int interface_index(struct msghdr msg)
+{
+	struct cmsghdr *cmsg;
+	struct in6_pktinfo *pi;
+	int ifidx = 0;
+
+	for (cmsg = CMSG_FIRSTHDR(&msg); cmsg != NULL; cmsg = CMSG_NXTHDR(&msg, cmsg)) {
+		if (cmsg->cmsg_level != IPPROTO_IPV6 || cmsg->cmsg_type != IPV6_PKTINFO)
+			continue;
+		pi = (struct in6_pktinfo *) CMSG_DATA(cmsg);
+		ifidx = pi->ipi6_ifindex;
+	}
+
+	return ifidx;
+}
+
+
+void * process_multicast_address_record(struct mar *mrec, int ifidx)
 {
 	char straddr[INET6_ADDRSTRLEN];
 	struct in6_addr addr;
 	struct in6_addr *src;
+	struct mld_group_t *g;
 	uint16_t source_count;
 
 	source_count = ntohs(mrec->mar_sources);
+	addr = mrec->mar_address;
+
+	/* find/allocate record for this address/interface combo */
+	g = group_record(addr, ifidx);
+	assert(g);
 
 	switch (mrec->mar_type) {
 		case MODE_IS_INCLUDE:
+			if (g->mode != FILTER_MODE_INCLUDE)
+				fprintf(stderr, ANSI_COLOR_YELLOW "");
+			g->mode = FILTER_MODE_INCLUDE;
 			fprintf(stderr, "\n\tMODE_IS_INCLUDE ");
 			break;
 		case MODE_IS_EXCLUDE:
+			if (g->mode != FILTER_MODE_EXCLUDE)
+				fprintf(stderr, ANSI_COLOR_YELLOW "");
+			g->mode = FILTER_MODE_EXCLUDE;
 			fprintf(stderr, "\n\tMODE_IS_EXCLUDE ");
 			break;
 		case CHANGE_TO_INCLUDE_MODE:
+			if (g->mode != FILTER_MODE_INCLUDE)
+				fprintf(stderr, ANSI_COLOR_YELLOW "");
+			g->mode = FILTER_MODE_INCLUDE;
 			fprintf(stderr, "\n\tCHANGE_TO_INCLUDE_MODE ");
 			if (source_count == 0) fprintf(stderr, " INCLUDE NULL => PART");
 			break;
 		case CHANGE_TO_EXCLUDE_MODE:
+			if (g->mode != FILTER_MODE_EXCLUDE)
+				fprintf(stderr, ANSI_COLOR_YELLOW "");
+			g->mode = FILTER_MODE_EXCLUDE;
 			fprintf(stderr, "\n\tCHANGE_TO_EXCLUDE_MODE ");
 			if (source_count == 0) fprintf(stderr, " EXCLUDE NULL => JOIN(ASM) ");
 			break;
 		case ALLOW_NEW_SOURCES:
+			fprintf(stderr, ANSI_COLOR_GREEN "");
 			fprintf(stderr, "\n\tALLOW_NEW_SOURCES ");
 			break;
 		case BLOCK_OLD_SOURCES:
+			fprintf(stderr, ANSI_COLOR_RED "");
 			fprintf(stderr, "\n\tBLOCK_OLD_SOURCES ");
 			break;
 		default:
@@ -106,7 +231,6 @@ void * process_multicast_address_record(struct mar *mrec)
 			break;
 	}
 
-	addr = mrec->mar_address;
 	inet_ntop(AF_INET6, addr.s6_addr, straddr, INET6_ADDRSTRLEN);
 	fprintf(stderr, "\n\tmulticast group addr=%s", straddr);
 	fprintf(stderr, " mode=%i", mrec->mar_type);
@@ -118,9 +242,10 @@ void * process_multicast_address_record(struct mar *mrec)
 	for (int i = 0; i < source_count; i++) {
 		inet_ntop(AF_INET6, (++src)->s6_addr, straddr, INET6_ADDRSTRLEN);
 		fprintf(stderr, "\n\t\tsource=%s", straddr);
+		/* TODO: write source addresses */
 	}
 
-	/* TODO: update MLD2 state machine */
+	fprintf(stderr, ANSI_COLOR_RESET "");
 
 	/* return pointer to next record */
 	src++;
@@ -130,12 +255,12 @@ void * process_multicast_address_record(struct mar *mrec)
 int main(int argc, char **argv)
 {
 	int ret = 0;
-	int s = 0;
 	int joins = 0;
+	int ifidx = 0;
 	ssize_t bytes = 0;
 	struct ipv6_mreq req;
 	struct msghdr msg;
-	struct iovec iov;
+	struct iovec iov[1];
 	struct icmp6_hdr *icmp6;
 	struct ifaddrs *ifaddr, *ifa;
 	struct mar *mrec;
@@ -144,8 +269,12 @@ int main(int argc, char **argv)
 	char buf_name[BUFSIZE];
 	uint16_t rec;
 
-	s = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
-	assert(s != 0);
+	sock = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6);
+	assert(sock != 0);
+
+	/* request ancilliary control data */
+	int opt = 1;
+	setsockopt(sock, IPPROTO_IPV6, IPV6_RECVPKTINFO, &opt, sizeof(opt));
 
 	getifaddrs(&ifaddr);
 	for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
@@ -154,7 +283,7 @@ int main(int argc, char **argv)
 		/* join "all MLDv2-capable routers" group */
 		inet_pton(AF_INET6, MLD2_CAPABLE_ROUTERS, &(req.ipv6mr_multiaddr));
 		req.ipv6mr_interface = if_nametoindex(ifa->ifa_name);
-		ret = setsockopt(s, IPPROTO_IPV6, IPV6_ADD_MEMBERSHIP, &req, sizeof(req));
+		ret = setsockopt(sock, IPPROTO_IPV6, IPV6_ADD_MEMBERSHIP, &req, sizeof(req));
 		if (ret != -1) {
 			fprintf(stderr, "listening on interface %s\n", ifa->ifa_name);
 			joins++;
@@ -167,18 +296,20 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
-	iov.iov_base = buf_recv;
-	iov.iov_len = sizeof(buf_recv);
+	iov[0].iov_base = buf_recv;
+	iov[0].iov_len = BUFSIZE;
 	msg.msg_control = buf_ctrl;
-	msg.msg_controllen = sizeof(BUFSIZE);
+	msg.msg_controllen = BUFSIZE;
 	msg.msg_name = buf_name;
-	msg.msg_namelen = sizeof(BUFSIZE);
-	msg.msg_iov = &iov;
+	msg.msg_namelen = BUFSIZE;
+	msg.msg_iov = iov;
 	msg.msg_iovlen = 1;
 	msg.msg_flags = 0;
 
+	signal(SIGINT, handle_sigint);
+
 	for (;;) {
-		bytes = recvmsg(s, &msg, 0);
+		bytes = recvmsg(sock, &msg, 0);
 		if (bytes < 0) {
 			perror("recvmsg");
 			if (errno == EINTR)
@@ -211,6 +342,9 @@ int main(int argc, char **argv)
 				case MLD2_LISTEN_REPORT:
 					fprintf(stderr, "MLD2_LISTEN_REPORT");
 
+					ifidx = interface_index(msg);
+					fprintf(stderr, " iface=%i", ifidx);
+
 					/* number of mcast address records */
 					rec = ntohs(icmp6->icmp6_data16[1]);
 					fprintf(stderr, " (records=%i)", rec);
@@ -222,7 +356,7 @@ int main(int argc, char **argv)
 						/* don't read beyond end of packet */
 						assert((void *)mrec <= (void *)(icmp6 + bytes));
 
-						mrec = process_multicast_address_record(mrec);
+						mrec = process_multicast_address_record(mrec, ifidx);
 					}
 					break;
 				default:
@@ -232,6 +366,6 @@ int main(int argc, char **argv)
 			fprintf(stderr, "\n");
 		}
 	}
-		
+
 	return 0;
 }
